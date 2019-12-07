5bda851b2b2bab4e4337bd703e74735e2305a40a
Sam Stuver <samuelstuver@gmail.com>
2019-12-07 15:31:03 -0700

    Change with Sam Dec 8

--- conditionals.py
+++ conditionals.py
@@ -1,14 +1,17 @@
 #CONDITIONALS
 
 first=input('first name     ')
-if first == 'Rich':
+if first.lower() == 'rich':
     print('Hi Rich')
 elif first == 'Joe':
      print('hi Joe')
 elif first == 'Ralph':
     print ('Hi Ralph')
+elif first == 'Sam':
+    print('Hi Sam')
 else:
-    print ('You are not on my list')    
+    print ('You are not on my list')
+    
 
 
 
