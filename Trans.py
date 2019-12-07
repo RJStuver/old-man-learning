e916728000ec838d5791e71f7f04039d5ba9500c
Sam Stuver <samuelstuver@gmail.com>
2019-12-07 15:41:37 -0700

    Test commit

--- Trans.py
+++ Trans.py
@@ -5,7 +5,7 @@
 alienDictionary = {'we': 'vorag', 'come': 'thang', 'in': 'zon', 'peace': 'arga', 'hello': 'kodar', 'can': 'znak', 'i': 'az', 'borrow': 'lifit', 'some': 'zum', 'rocket': 'upgoman',
                    'fuel': 'kakboom', 'please': 'zelpin', 'dont': 'baaaaaaaaarn', 'shoot': 'flabil', 'welcome': 'unkip', 'our': 'mandig', 'now': 'brang', 'alien': 'marangan', 'overlords': 'bap'}
 
-englishPhrase = input('Please enter an english worr or phrase to translate: ')
+englishPhrase = input('Please enter an english word or phrase to translate: ')
 englishWords = englishPhrase.lower().split()
 
 alienWords = []
