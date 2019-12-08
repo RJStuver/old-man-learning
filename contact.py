3448420b73ae5ec4a6f14ed473065e92fa7d5568
Sam Stuver <samuelstuver@gmail.com>
2019-12-07 18:55:31 -0700

    stuff

--- contact.py
+++ contact.py
@@ -1,4 +1,8 @@
 #CONTACT
+import csv
+
+
+
 
 first=input('first Name     ')
 
@@ -12,10 +16,37 @@ state=input('State   ')
 
 zip=input('Zip     ')
 
+person = {'first':first,'last':last,'address':add, 'city':city,'state':state,'zip':zip}
+
+print()
 print(last+', '+first)        #Print to screen in business card format
 print (add)     
 print(city+', '+state+'  '+zip)
-      
+print()
+
+print(person['last']+', ' + person['first'])
+print (person['address'])
+print(person['city']+','+person['state']+','+person['zip'])
+
+
+answer = input('Correct, Y/N? ')
+while answer !='y':
+    for field in person:
+        edit=input(person[field]+'  ')
+        if edit!='':
+            person[field]=edit
+    answer = input('Correct, Y/N? ')       
+        
+          
+
+
+
+
+#if: correct then save alphabetically using dictionary
+#if not return to top
+#Check for duplicate   
+#ass anuther file?
+     
 
 
                
